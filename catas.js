174b7bb9249a9a4fabc735ae271e861359c6ae46
UHanczar <u.hanczar@gmail.com>
2017-01-20 23:02:09 +0300

    20-01-2017

--- catas.js
+++ catas.js
@@ -50,3 +50,99 @@ function even_or_odd(number) {
 }
 
 // even_or_odd(4);
+
+// range from min to max with step
+function range(start = 0, step = 1, stop) {
+  let res = [];
+
+  let args = [];
+  for(let i = 0; i < arguments.length; i++) {
+    args[i] = arguments[i];
+  }
+  // console.log(args.length);
+  if(args.length === 1) {
+    start = 1;
+    step = 1;
+    stop = args[0];
+  } else if(args.length === 2) {
+    start = args[0];
+    step = 1;
+    stop = args[1];
+  } else {
+    start = args[0];
+    step = args[1];
+    stop = args[2];
+  }
+
+
+  while(start <= stop) {
+
+    res.push(start);
+    start += step
+  }
+  console.log(res);
+  return res;
+}
+
+//range(5);
+//range(3,7);
+//range(2,3,15);
+
+// boolean logic from scratch
+const or = (a, b) => {
+  // your code here :)
+  if(a && b) {
+    return true;
+  } else if(!a && !b){
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const xor = (a, b) => {
+  if(a) {
+    if(b) {
+      return false;
+    } else {
+      return true;
+    }
+  } else if(b) {
+    if(a) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
+
+};
+
+
+// cut '!' and put it at the end
+function remove(s){
+  //coding and coding....
+  let newStr = '';
+  let excl = '';
+  let res = '';
+  for(let i = 0; i < s.length; i++) {
+    if(s[i] === '!') {
+      // excl = s.slice(s[i], s[i+1]);
+      // console.log(excl);
+      // newStr += excl;
+      // console.log(newStr);
+      excl += s[i];
+      //console.log(s[i]);
+      s = s.replace(s[i], '');
+
+    }
+
+  }
+  console.log(excl);
+  console.log(s);
+  res = s.concat(excl);
+  return res;
+}
+
+//remove("Hi!fsa sfa! fja;f!");
