423c3425cb3bfe3a56293517401ef548d51fb572
UHanczar <u.hanczar@gmail.com>
2017-02-01 23:13:58 +0300

    function, which returns the sum of a sequence of integers

--- catas-febriary.js
+++ catas-febriary.js
@@ -14,3 +14,24 @@ function changeNumber(str) {
 }
 
 // changeNumber('1234567890575757577');
+
+// function, which returns the sum of a sequence of integers
+const sequenceSum = (begin, end, step) => {
+  // May the Force be with you
+  // May the Force be with you
+  let counter = begin;
+  if(begin >= end) {
+    return 0;
+  } else if((end - begin) < step) {
+    return begin;
+  }
+  console.log(begin, end, step);
+  while(begin < end - (step - 1)) {
+    begin += step;
+    counter += begin;
+  }
+
+  return counter;
+};
+
+sequenceSum(2, 6, 2);
