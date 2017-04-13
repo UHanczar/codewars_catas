4d2bec7d66b8492aab51007f87841e0c518a6e9b
UHanczar <u.hanczar@gmail.com>
2017-04-14 00:41:50 +0300

    14-04-2017

--- aprel_catas.js
+++ aprel_catas.js
@@ -42,5 +42,54 @@ function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) {
 
 // areEquallyStrong(10,5,5,10);
 
+// sort array numbers by multiplying them on their index
+function sortByValueAndIndex(array) {
+  let counter = 0;
+  let res = array.map(x => {
+    counter++;
+    return {
+      pverState: x,
+      currState: x * counter
+    };
+  }).sort((a, b) => a.currState - b.currState).map(x => x.pverState);
+
+  return res;
+}
+
+// sortByValueAndIndex([ 23, 2, 3, 4, 5 ]);
 
 
+// split array on pair and add to single characters '_'
+function solution(str){
+  let arr = str.split('');
+  console.log(arr);
+  let res = [];
+  for(let i = 0, length = arr.length; i < length; i +=2) {
+    let a = arr.splice(0, 2).join('');
+    res.push(a);
+  }
+
+  res = res.map(x => x.length < 2 ? x + '_' : x);
+  return res;
+}
+
+// solution('abcde');
+
+// sum average of two arrays
+function sumAverage(arr) {
+  let res = arr.map(x => {
+    let length = x.length;
+    return x.reduce((counter, x) => counter += x, 0) / length;
+  }).reduce((counter, x) => counter += x, 0);
+
+  return Math.floor(res);
+}
+
+// sumAverage([[3, 4, 1, 3, 5, 1, 4], [21, 54, 33, 21, 76]]);
+
+// create phone number from array
+function createPhoneNumber(numbers){
+  return '(' + numbers.splice(0, 3).join('') + ')' + ' ' + numbers.splice(0, 3).join('') + '-' + numbers.splice(0, 4).join('');
+}
+
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
--- fcc_intermediate_algs.js
+++ fcc_intermediate_algs.js
@@ -42,3 +42,65 @@ function diffArray(arr1, arr2) {
 }
 
 // diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
+
+// converter to roman numerals
+const convertToRoman = (num) => {
+  const arabNumeral = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+  const romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+
+  let romanNumber = '';
+
+  for(let i = 0; i < arabNumeral.length; i++) {
+    while(arabNumeral[i] <= num) {
+      romanNumber += romanNumeral[i];
+      num -= arabNumeral[i];
+    }
+  }
+  return romanNumber;
+};
+
+// convertToRoman(36);
+
+// return array of values corresponding to course
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  let arr = [];
+  // Only change code below this line
+  let keys = Object.keys(source);
+  console.log(keys);
+
+  arr = collection.filter(x => {
+    for(let i = 0; i < keys.length; i++) {
+      if(!x.hasOwnProperty(keys[i]) || x[keys[i]] !== source[keys[i]]) {
+        return false;
+      }
+    }
+    return true;
+  });
+  // Only change code above this line
+  return arr;
+}
+
+// whatIsInAName([{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }, { "a": 1 }], { "a": 1, "b": 2 });
+
+
+//  replace word before to word after, minding uppercase
+function myReplace(str, before, after) {
+  let res = '';
+  let checkCapital = before.charCodeAt();
+  console.log(checkCapital);
+  if(checkCapital > 96 && checkCapital < 123) {
+    res = str.replace(before, after);
+  } else {
+    let firstLetter = after.slice(0, 1).charCodeAt() - 32;
+    firstLetter = String.fromCharCode(firstLetter);
+    console.log(firstLetter);
+    let fixedAfter = firstLetter + after.slice(1);
+    res = str.replace(before, fixedAfter);
+  }
+
+  return res;
+}
+
+// myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped");
+
